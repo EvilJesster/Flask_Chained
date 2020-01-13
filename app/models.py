afed31ec10e32fb7460383beb14c5f8e6c4f28e3
Ivan Galakhov <igalakhov.nyc@gmail.com>
2020-01-13 09:52:01 -0500

    vibe check

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    boards = db.relationship('SudokuSaveState', back_populates='user')
 
     def __init__(self, username, email, password):
         self.username = username
@@ -21,14 +22,34 @@ class Sudoku(db.Model):
     solved_str = db.Column(db.String(81), nullable=True)
     uuid = db.Column(db.String(100), nullable=False)
 
+    save_states = db.relationship('SudokuSaveState', back_populates='board')
+
     def __init__(self, uuid, solve, unsolve):
         self.uuid = uuid
         self.solved_str = solve
         self.unsolved_str = unsolve
 
-    @property
-    def is_active():
-        return (unsolved_str is not None and solve_str is not None)
+# save state
+# actually an association table
+
+
+
+class SudokuSaveState(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    # columns
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    sudoku_id = db.Column(db.Integer, db.ForeignKey('sudoku.id'), primary_key=True)
+    number_str = db.Column(db.String(81), nullable=False, default='_'*81)
+    pencilmark_str = db.Column(db.String(729), nullable=False, default='_'*791)
+
+    user = db.relationship(User, back_populates='boards')
+    board = db.relationship(Sudoku, back_populates='save_states')
+
+
+    def __init__(self, user, sudoku):
+        pass
+    
 
 class Leaderboard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
